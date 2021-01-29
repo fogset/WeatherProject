40488ff9588e95e2f80986f9b28a8204dea5ddfc
fogset <zhangth1990@hotmail.com>
2021-01-29 00:13:31 -0800

    use body parser to parse post requests to the server

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const https = require("https");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=kelowna&appid=a59ed37c17c8e35b7e481c734f48aed7&units=metric";
+  res.sendFile(__dirname + "/index.html")
+})
+app.post("/", function(req, res){
+  console.log(req.body.cityName);
+  const query = req.body.cityName
+  const apiKey = "a59ed37c17c8e35b7e481c734f48aed7"
+  const unit = "metric"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units=" + unit;
 
   https.get(url, function(response) {
     console.log(response.statusCode);
@@ -15,12 +25,11 @@ app.get("/", function(req, res) {
       const weatherIcon = weatherData.weather[0].icon
       const imageURL = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"
       res.write("<p>The weather now is " + weatherDescription + "<p>");
-      res.write("<h1>The temperature in Kelowna is " + temp + "degrees Celcius.</h1> ");
+      res.write("<h1>The temperature in "+ query +" is " + temp + " degrees Celcius.</h1> ");
       res.write("<img src=" + imageURL + ">");
       res.send();
     })
   })
-
 })
 
 
